d4685b3f041dd9ab593c6bff4f6ee3d36dbb17f3
JF <jfan9988@gmail.com>
2020-03-30 21:29:07 +1100

    pure threejs test with env map

--- src/components/threejs/MainCanvas.js
+++ src/components/threejs/MainCanvas.js
@@ -1,7 +1,6 @@
-import React from "react";
-import { Canvas } from "react-three-fiber";
-import Box from "./Box";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
+import * as THREE from "three";
 
 const StyledDiv = styled.div`
   height: 100vh;
@@ -9,16 +8,60 @@ const StyledDiv = styled.div`
 `;
 
 const MainCanvas = () => {
-  return (
-    <StyledDiv>
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
-        <Box position={[1.2, 0, 0]} />
-      </Canvas>
-    </StyledDiv>
-  );
+  useEffect(() => {
+    // === THREE.JS CODE START ===
+    var scene = new THREE.Scene();
+    var r = "https://threejs.org/examples/textures/cube/Bridge2/";
+    var urls = [
+      r + "posx.jpg",
+      r + "negx.jpg",
+      r + "posy.jpg",
+      r + "negy.jpg",
+      r + "posz.jpg",
+      r + "negz.jpg"
+    ];
+    var skyMap = new THREE.CubeTextureLoader().load(urls);
+    scene.background = skyMap;
+    skyMap.mapping = THREE.CubeRefractionMapping;
+
+    var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(100, 350, 250);
+    directionalLight.castShadow = true;
+    scene.add(directionalLight);
+
+    var camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+
+    var selfElement = document.getElementById("mainCanvas");
+    var renderer = new THREE.WebGLRenderer();
+    selfElement.appendChild(renderer.domElement);
+    renderer.setSize(selfElement.clientWidth, selfElement.clientHeight);
+
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshLambertMaterial({
+      color: 0xffffff,
+      envMap: skyMap,
+      refractionRatio: 0.99
+    });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+    camera.position.z = 5;
+
+    var animate = function() {
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    };
+
+    animate();
+    // === THREE.JS EXAMPLE CODE END ===
+  }, []);
+  return <StyledDiv id="mainCanvas"></StyledDiv>;
 };
 
 export default MainCanvas;
