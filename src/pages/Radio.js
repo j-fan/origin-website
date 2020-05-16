c71c6ab39d31a53918a53b0e973442f599137a34
CallumHoward <callumh@cse.unsw.edu.au>
2020-05-16 19:06:57 +1000

    removed unused imports

--- src/pages/Radio.js
+++ src/pages/Radio.js
@@ -1,22 +1,28 @@
 import React from "react";
 import Page from "../components/Page";
 import PropTypes from "prop-types";
-import AudioPlayer from "react-h5-audio-player";
-import "react-h5-audio-player/lib/styles.css";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 20%;
+`;
 
 const Radio = ({ isVisible, direction }) => {
   return (
     <Page isVisible={isVisible} direction={direction}>
       <h1>Radio</h1>
-      <div style={{ display: "flex", justifyContent: "center", marginTop: "20%" }}>
+      <StyledDiv>
         <audio controls>
           <source
+            autoplay
             src="http://139.218.99.233:3141/radio/8000/radio.mp3?1589614808"
             type="audio/mpeg"
           />
           Your browser does not support the audio element.
         </audio>
-      </div>
+      </StyledDiv>
     </Page>
   );
 };
