d8e332a61830ce921a19a33c765fce0909eeda5c
JF <jfan9988@gmail.com>
2020-06-21 20:25:02 +1000

    update subvirtual image

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -18,13 +18,33 @@ const StyledDiv = styled.div`
 const Gallery = ({ isVisible, direction }) => {
   return (
     <Page isVisible={isVisible} direction={direction}>
-      <h1>Gallery</h1>
+      <h1>Origin Virtual</h1>
+      <p id="OriginArtworks">
+        Origin (Virtual) is an interactive web exhibition by Sydney Media Arts
+        collective <a href="https://www.originart.xyz/">Origin</a>.
+        <br />
+        <br />
+        This exhibition was born out of the current environment which forced
+        artists to quickly adapt to a new normal in which the Virtual plane
+        becomes increasingly necessary to everyday life.
+        <br />
+        <br />
+        Origin (Virtual) features artworks that push the boundaries of websites
+        as a medium, with artists interrogating the applications of artificial
+        intelligence, generative art and audiovisual installations in the
+        virtual space.
+        <br />
+        <br />
+        In doing so, Origin seeks to deconstruct the traditional online gallery
+        experience whilst addressing how social interaction and spectacle has
+        morphed during our time inside.
+      </p>
       <StyledDiv>
         <GalleryCard
           title="Subvirtual"
           imageUrl="img/subvirtual.jpg"
           creator="Callum Howard"
-          link={"https://www.originart.xyz"}
+          link={"https://www.callumhoward.com/origin/subvirtual"}
           description="Subvirtual Gallery is a gallery full of blank canvases, but as visitors explore the mysteries of the space they will find the gallery's artworks appear everywhere but on the canvas."
         />
         <GalleryCard
@@ -45,7 +65,7 @@ const Gallery = ({ isVisible, direction }) => {
           title="Meeting Hill"
           imageUrl="img/meetingHill.jpg"
           creator="Patrick Younis Jordan East"
-          link={"https://www.originart.xyz"}
+          link={"http://tildevisual.tv/"}
           description="Meeting Hill is a web based virtual hilltop, decorated with furniture and a sound system, where visitors can come to listen to a live curated online radio station."
         />
       </StyledDiv>
