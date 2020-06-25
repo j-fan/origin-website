28315e6780a2a62b258a7bcd31d0ca6a1d527bfb
CallumHoward <callum.howard@safetyculture.io>
2020-06-26 03:20:40 +1000

    updated radio page

--- src/pages/Radio.js
+++ src/pages/Radio.js
@@ -9,21 +9,31 @@ const StyledDiv = styled.div`
   margin-top: 20%;
 `;
 
+const StyledImage = styled.img`
+  margin-top: 20%;
+  width: 100%;
+  height: 100%;
+`;
+
 const Radio = ({ isVisible, direction }) => {
   return (
     <Page isVisible={isVisible} direction={direction}>
       <h1>Radio</h1>
+    <h2>Origin (Virtual) : Meeting Hill</h2>
       <StyledDiv>
         <audio controls>
           <source
             autoplay
             crossorigin="anonymous"
-            src="https://radio.originart.xyz/radio/8000/radio.mp3?1589621110"
+            src="http://167.172.134.69/radio/8000/radio.mp3"
             type="audio/mpeg"
           />
-          Your browser does not support the audio element.
+          Your browser does not support the audio element. Try using Chrome!
         </audio>
       </StyledDiv>
+
+      <StyledImage src="img/MeetingHillprogramming.jpg" alt="programme"/>
+
     </Page>
   );
 };
